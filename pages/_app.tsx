16e4cb1185e914cacf6577bd44de183f8564bdbf
Ph4ntom3 <nicholas.roether@t-online.de>
2020-11-27 13:19:35 +0100

    Import default styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { AppProps } from "next/dist/next-server/lib/router/router"
 
+import "../styles/styles.css";
+
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
   return <Component {...pageProps} />
 }
