a669f192742f3f889d20d91bf3425636d17f4316
nicholas-roether <nicholas.roether@t-online.de>
2020-11-27 23:15:36 +0100

    Use API consumption

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import { server } from "../config";
 import { makeStyles } from "@material-ui/core";
 import { TimeTable } from "gyloh-webuntis-api";
 import React from "react";
 import Page from "../components/page";
-import TimeTableView from "../components/time_table_view";
+import TimeTableTableView from "../components/time_table_table_view";
+import { getCurrentTables } from "../api_consume/consume_table";
 
 const useStyles = makeStyles({
   tableHead: {
@@ -18,12 +18,12 @@ const Home: React.FC = () => {
   const [table, setTable] = React.useState<TimeTable | null>(null);
   const classes = useStyles();
   if(table == null) {
-    fetch(`${server}/api/tables/current?num=1`).then(res => res.json().then(data => setTable(data[0])));
+    getCurrentTables(2).then(tables => setTable(tables[1]));
     return <Page title="Dashboard" />
   }
   return (
     <Page title="Dashboard">
-      <TimeTableView table={table} />
+      <TimeTableTableView table={table} />
     </Page>
   )
 }
