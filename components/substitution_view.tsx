9694540abd9ee7af838f82915865de25917c9615
nicholas-roether <nicholas.roether@t-online.de>
2020-11-27 23:15:12 +0100

    Incorporate text output

--- components/substitution_view.tsx
+++ components/substitution_view.tsx
@@ -3,16 +3,18 @@ import React from "react";
 
 export interface SubstitutionViewProps<T> {
 	value: T | Substitution<T>;
-	children: (current?: T, subst?: T) => React.ReactElement;
+	current: (current?: T) => React.ReactElement<any, any> | string | number | null | undefined;
+	subst: (subst?: T) => React.ReactElement<any, any> | string | number | null | undefined;
 }
 
 function isSubstitution<T>(val: any): val is Substitution<T> {
 	return typeof val === "object" && "current" in val &&  "subst" in val;
 }
 
-function SubstitutionView<T>({ value, children }: SubstitutionViewProps<T>): React.ReactElement {
-	if(isSubstitution(value)) return children(value.current || undefined, value.subst || undefined);
-	return children(value);
+function SubstitutionView<T>({ value, current, subst }: SubstitutionViewProps<T>): React.ReactElement {
+	if(isSubstitution(value)) 
+		return <span>{current(value.current || undefined)} (<s>{subst(value.subst || undefined)}</s>)</span>;
+	return <span>{current(value)}</span>
 }
 
 export default SubstitutionView;
