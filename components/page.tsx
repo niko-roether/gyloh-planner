c01b626156089b8642a8c2db911a8042a16b15c7
Ph4ntom3 <nicholas.roether@t-online.de>
2020-11-27 19:52:12 +0100

    Add maxHeight to page

--- components/page.tsx
+++ components/page.tsx
@@ -1,19 +1,27 @@
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, makeStyles } from "@material-ui/core";
 import React from "react";
 import PageBar from "./page_bar";
 
-
+const useStyles = makeStyles(theme => ({
+	page: {
+		height: "100vh",
+	},
+	main: {
+		// height: `calc(100vh - ${theme.mixins.toolbar.height})`
+	}
+}));
 
 export interface PageProps {
 	title: string;
 }
 
 const Page: React.FC<PageProps> = ({ title, children }) => {
+	const classes = useStyles();
 	return (
-		<div className="page">
+		<div className={classes.page}>
 			<CssBaseline />
 			<PageBar title={title} />
-			{children}
+			<main className={classes.main}>{children}</main>
 		</div>
 	);
 }
