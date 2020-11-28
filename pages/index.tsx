c22f6f6182122205d54b29312afb918630056555
nicholas-roether <nicholas.roether@t-online.de>
2020-11-28 22:04:50 +0100

    Show multiple tables

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,14 @@ import React from "react";
 import Page from "../components/page";
 import { getCurrentTables } from "../api_consume/consume_table";
 import TimeTableView from "../components/time_table_view";
+import PromiseBuilder from "../components/promise_builder";
 
 const Home: React.FC = () => {
-  const [table, setTable] = React.useState<TimeTable | null>(null);
-  if(table == null) {
-    getCurrentTables(1).then(tables => setTable(tables[0]));
-    return <Page title="Dashboard" />
-  }
   return (
     <Page title="Dashboard">
-      <TimeTableView table={table} />
+      <PromiseBuilder promise={getCurrentTables(3)}>
+        {tables => tables && tables.map(table => <TimeTableView table={table} />)}
+      </PromiseBuilder>
     </Page>
   )
 }
