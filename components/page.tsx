a772199117a0a34025f09893c19b27e6779f5a4f
nicholas-roether <nicholas.roether@t-online.de>
2020-11-28 02:37:33 +0100

    Remove unecessary styles

--- components/page.tsx
+++ components/page.tsx
@@ -2,26 +2,17 @@ import { CssBaseline, makeStyles } from "@material-ui/core";
 import React from "react";
 import PageBar from "./page_bar";
 
-const useStyles = makeStyles(theme => ({
-	page: {
-		height: "100vh",
-	},
-	main: {
-		// height: `calc(100vh - ${theme.mixins.toolbar.height})`
-	}
-}));
 
 export interface PageProps {
 	title: string;
 }
 
 const Page: React.FC<PageProps> = ({ title, children }) => {
-	const classes = useStyles();
 	return (
-		<div className={classes.page}>
+		<div>
 			<CssBaseline />
 			<PageBar title={title} />
-			<main className={classes.main}>{children}</main>
+			<main>{children}</main>
 		</div>
 	);
 }
