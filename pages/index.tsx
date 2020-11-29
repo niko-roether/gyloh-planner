cd5eb3c2fa0f4b00ff3e53e92efa92ace1135f0a
nicholas-roether <nicholas.roether@t-online.de>
2020-11-29 20:05:06 +0100

    Use swr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,32 @@
-import { TimeTable } from "gyloh-webuntis-api";
 import React from "react";
 import Page from "../components/page";
 import { getCurrentTables } from "../api_consume/consume_table";
 import TimeTableView from "../components/time_table_view";
-import PromiseBuilder from "../components/promise_builder";
 import ResponsiveListView from "../components/responsive_list_view";
+import { Box, CircularProgress, Container } from "@material-ui/core";
+import useSWR from "swr";
 
 const Home: React.FC = () => {
+  const swr = useSWR("dashboard-tables", () => getCurrentTables(3));
+
+  let content
+  if(swr.error) content = (
+    <Container>Ein Fehler ist aufgetreten: {swr.error}</Container>
+  )
+  else if(!swr.data) content = (
+    <Box mt={6} marginX="auto" textAlign="center">
+      <CircularProgress />
+    </Box>
+  )
+  else content = (
+    <ResponsiveListView titles={swr.data.map(table => table.date.toLocaleDateString("de-DE"))}>
+      {swr.data.map((table, i) => <TimeTableView table={table} key={i} />)}
+    </ResponsiveListView>
+  );
+
   return (
     <Page title="Dashboard">
-      <PromiseBuilder promise={getCurrentTables(3)}>
-        {tables => tables && 
-          <ResponsiveListView titles={tables.map(table => table.date.toLocaleDateString("de-DE"))}>
-            {tables.map((table, i) => <TimeTableView table={table} key={i} />)}
-          </ResponsiveListView>
-        }
-      </PromiseBuilder>
+      {content}
     </Page>
   )
 }
