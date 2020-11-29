73f17931308ba5ccefe956d553e645f6a02e2ed4
nicholas-roether <nicholas.roether@t-online.de>
2020-11-29 06:40:52 +0100

    Use API utilities

--- pages/api/tables/current.ts
+++ pages/api/tables/current.ts
@@ -1,22 +1,31 @@
 import { NextApiHandler } from "next";
 import { GylohWebUntis } from "gyloh-webuntis-api";
+import { apiError, apiRawResponse, apiResponse, jsonEncodeTable, requireMethods, requireQueryArg, runMiddleware } from "../../../util/api_util";
+import Cors from "cors";
+
+const legalMethods = ["GET", "HEAD"];
+
+const cors = Cors({
+	methods: legalMethods
+});
 
 const handler: NextApiHandler = async (req, res) => {
-	const end = (code: number) => {
-		res.statusCode = code;
-		res.end();
-	}
-	if(req.method !== "GET") return end(405);
-	const numStr = req.query["num"];
-	if(!numStr || typeof numStr !== "string") return end(400);
-	const num = Number.parseInt(numStr);
-	if(num === NaN) return end(400);
-
-	const plans = await GylohWebUntis.getCurrentTables(num);
-	
-	res.statusCode = 200;
 	res.setHeader("Content-Type", "application/json");
-	res.end(JSON.stringify(plans));
+
+	try {
+		await runMiddleware(req, res, cors);
+	} catch(e) {
+		res.status(500).end(apiError("CORS process failed"));
+	}
+
+	if(!requireMethods(req, res, legalMethods)) return;
+	const numStr = requireQueryArg(req, res, "num");
+	if(!numStr) return;
+	const num = Number.parseInt(numStr);
+	if(num === NaN) return res.status(400).end(apiError("Argument 'num' must be a number"));
+
+	const tables = await GylohWebUntis.getCurrentTables(num);
+	res.status(200).end(apiRawResponse(`[${tables.map(table => jsonEncodeTable(table)).join(",")}]`));
 }
 
 export default handler;
