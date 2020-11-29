b938a9f1383b9842d9a50aac6d4b807e2f14723e
nicholas-roether <nicholas.roether@t-online.de>
2020-11-30 00:14:21 +0100

    Create feedback page

---
+++ src/pages/feedback.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Heading from "../components/heading";
+
+const Feedback: React.FC = () => {
+	return (
+		<Heading variant="h3">Feedback</Heading>
+	)
+}
+
+export default Feedback;
