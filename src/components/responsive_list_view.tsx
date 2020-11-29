3f27c7926c1b77445d72c84c8b3b0191a7a31113
nicholas-roether <nicholas.roether@t-online.de>
2020-11-30 00:12:59 +0100

    Use Hidden components

--- src/components/responsive_list_view.tsx
+++ src/components/responsive_list_view.tsx
@@ -1,4 +1,4 @@
-import { Button, makeStyles, MobileStepper, Paper, Step, StepButton, Stepper, useMediaQuery, useTheme } from "@material-ui/core";
+import { Button, Hidden, makeStyles, MobileStepper, Paper, Step, StepButton, Stepper, useTheme } from "@material-ui/core";
 import { Breakpoint } from "@material-ui/core/styles/createBreakpoints";
 import { CalendarToday, KeyboardArrowLeft, KeyboardArrowRight } from "@material-ui/icons";
 import React from "react";
@@ -30,7 +30,6 @@ export interface ResponsiveListViewProps<P> {
 
 const ResponsiveListView: React.FC<ResponsiveListViewProps<any>> = ({component = "div", breakpoint = "sm", children, componentProps, titles}) => {
 	const theme = useTheme();
-	const mobileView = useMediaQuery(theme.breakpoints.down(breakpoint));
 	const [index, setIndex] = React.useState<number>(0);
 	const classes = useStyles();
 
@@ -44,52 +43,52 @@ const ResponsiveListView: React.FC<ResponsiveListViewProps<any>> = ({component =
 	return (
 		<Component {...componentProps}>
 			<Paper square>
-				{mobileView
-					? (
-						<MobileStepper 
-							variant="text"
-							activeStep={index}
-							steps={elements.length}
-							className={classes.mobileStepper}
-							nextButton={
-								<Button size="small" onClick={toNext} disabled={index === elements.length - 1}>
-								Next
-								{theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-								</Button>
-							}
-							backButton={
-								<Button size="small" onClick={toPrev} disabled={index === 0}>
+				<Hidden mdUp>
+					<MobileStepper 
+						variant="text"
+						position="static"
+						activeStep={index}
+						steps={elements.length}
+						className={classes.mobileStepper}
+						nextButton={
+							<Button size="small" onClick={toNext} disabled={index === elements.length - 1}>
+							Next
+							{theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+							</Button>
+						}
+						backButton={
+							<Button size="small" onClick={toPrev} disabled={index === 0}>
+							{theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+							Back
+							</Button>
+						}
+					/>
+				</Hidden>
+				<Hidden smDown>
+					<nav className={classes.controls}  style={{maxWidth: 300 * elements.length}}>
+						<div className={classes.buttonContainer}>
+							<Button variant="contained" color="primary" onClick={toPrev} disabled={index === 0}>
 								{theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
 								Back
-								</Button>
-							}
-						/>
-					) : (
-						<nav className={classes.controls}  style={{maxWidth: 300 * elements.length}}>
-							<div className={classes.buttonContainer}>
-								<Button variant="contained" color="primary" onClick={toPrev} disabled={index === 0}>
-									{theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-									Back
-								</Button>
-							</div>
-							<Stepper className={classes.stepper} activeStep={index} alternativeLabel nonLinear>
-								{elements.map((_, i) => (
-									<Step key={i}>
-										<StepButton onClick={() => setIndex(i)} icon={<CalendarToday color={index === i ? "inherit" : "action"} />}>
-											{titles ? titles[i] : i}
-										</StepButton>
-									</Step>
-								))}
-							</Stepper>
-							<div className={classes.buttonContainer}>
-								<Button variant="contained" color="primary" onClick={toNext} disabled={index === elements.length - 1}>
-									Next
-									{theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-								</Button>
-							</div>
-						</nav>
-					)
-				}
+							</Button>
+						</div>
+						<Stepper className={classes.stepper} activeStep={index} alternativeLabel nonLinear>
+							{elements.map((_, i) => (
+								<Step key={i}>
+									<StepButton onClick={() => setIndex(i)} icon={<CalendarToday color={index === i ? "inherit" : "action"} />}>
+										{titles ? titles[i] : i}
+									</StepButton>
+								</Step>
+							))}
+						</Stepper>
+						<div className={classes.buttonContainer}>
+							<Button variant="contained" color="primary" onClick={toNext} disabled={index === elements.length - 1}>
+								Next
+								{theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+							</Button>
+						</div>
+					</nav>
+				</Hidden>
 			</Paper>
 			{elements.map((e, i) => (
 				<div key={i} style={{display: i === index ? "block" : "none"}}>
