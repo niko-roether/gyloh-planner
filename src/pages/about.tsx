c5f31147d202ded3a778762f081883d9be6ffb72
nicholas-roether <nicholas.roether@t-online.de>
2020-11-30 00:13:35 +0100

    Create about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Heading from "../components/heading";
+
+const About: React.FC = () => {
+	return (
+		<Heading variant="h3">Was Ist Das Hier?</Heading>
+	)
+}
+
+export default About;
