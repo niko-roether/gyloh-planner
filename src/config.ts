88e5cb86d39f60cde41665033f87d7be5bdfc7b9
nicholas-roether <nicholas.roether@t-online.de>
2020-11-29 20:35:08 +0100

    Update config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server: string = dev ? 'http://localhost:3000' : "http://nicholas-pc.fritz.box:3000"; // TODO deployment server
+export const server: string = dev ? 'http://localhost:3000' : "https://gyloh-planner.vercel.app/"; // TODO deployment server
