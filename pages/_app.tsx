094b9ea2a55440d3350cf91a4e05e793978a69d1
nicholas-roether <nicholas.roether@t-online.de>
2020-11-30 18:34:00 +0100

    Remove unecessary styles import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { AppProps } from "next/dist/next-server/lib/router/router"
 
-import "../styles/styles.css";
-
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
   return <Component {...pageProps} />
 }
