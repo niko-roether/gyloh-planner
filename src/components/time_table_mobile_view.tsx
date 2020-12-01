698c29d9b83166cd2c6a75aa8f590587feb5106d
Ph4ntom3 <nicholas.roether@t-online.de>
2020-12-01 12:36:16 +0100

    Implement data restructuring

--- src/components/time_table_mobile_view.tsx
+++ src/components/time_table_mobile_view.tsx
@@ -1,49 +1,45 @@
-import { Accordion, AccordionDetails, AccordionSummary, Card, makeStyles, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@material-ui/core";
-import { ExpandMore as ExpandMoreIcon } from "@material-ui/icons";
+import { Accordion, AccordionDetails, AccordionSummary, AppBar, Box, Card, Dialog, Divider, IconButton, List, ListItem, ListItemText, makeStyles, Slide, Table, TableBody, TableCell, TableContainer, TableRow, Toolbar, Typography } from "@material-ui/core";
+import { TransitionProps } from "@material-ui/core/transitions/transition";
+import { ArrowBack as ArrowBackIcon, Close as CloseIcon, ExpandMore as ExpandMoreIcon } from "@material-ui/icons";
+import { Class } from "gyloh-webuntis-api";
 import React from "react";
 import SubstitutionView from "./substitution_view";
-import { COLUMN_TITLES, TimeTableSubViewProps, TimeTableViewEntryProps } from "./time_table_view";
+import { COLUMN_TITLES, TimeTableEntryFields, TimeTableEntryFieldsForClass, TimeTableSubViewProps, TimeTableViewEntryProps } from "./time_table_view";
 
 const useStyles = makeStyles(theme => ({
-	list: {
-		margin: theme.spacing(3, 0)
-	},
-	topBar: {
-		display: "flex",
-		justifyContent: "space-between",
-	},
-	textWrapper: {
-		height: "100%",
-		flex: 4,
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "space-between"
-	},
-	subTextWrapper: {
-		width: "100%",
-		maxWidth: 200,
-		display: "inline-flex",
-		justifyContent: "space-between"
-	},
-	accordionSummary: {
+	summary: {
 		overflowX: "hidden",
+		width: "100%"
+	},
+	subject: {
+		flexBasis: "70%",
+		flexShrink: 0,
+	},
+	lesson: {
+		color: theme.palette.text.secondary
+	},
+	dialogContent: {
+		paddingTop: theme.spacing(3)
+	},
+	dialogHeading: {
+		paddingLeft: theme.spacing(1)
+	},
+	accordion: {
+		overflowX: "clip"
 	}
 }))
 
+interface TimeTableMobileViewEntryProps {
+	fields: TimeTableEntryFields
+}
 
-const TimeTableMobileViewEntry: React.FC<TimeTableViewEntryProps> = ({ fields }) => {
+const TimeTableMobileViewEntry: React.FC<TimeTableMobileViewEntryProps> = ({ fields }) => {
 	const classes = useStyles();
-
 	return (
-		<Accordion>
-			<AccordionSummary className={classes.accordionSummary} expandIcon={<ExpandMoreIcon />}>
-				<div className={classes.textWrapper}>
-					<Typography variant="subtitle1">{fields.class.longName}</Typography>
-					<span className={classes.subTextWrapper}>
-						<Typography variant="subtitle2">{fields.subject.longName}</Typography>
-						<Typography variant="subtitle2">{fields.lesson}</Typography>
-					</span>
-				</div>
+		<Accordion className={classes.accordion} square>
+			<AccordionSummary className={classes.summary} expandIcon={<ExpandMoreIcon />}>
+				<Typography className={classes.subject}>{fields.subject.longName}</Typography>
+				<Typography className={classes.lesson}>{fields.lesson}</Typography>
 			</AccordionSummary>
 			<AccordionDetails>
 				<TableContainer component={Card} variant="outlined">
@@ -73,21 +69,71 @@ const TimeTableMobileViewEntry: React.FC<TimeTableViewEntryProps> = ({ fields })
 	);
 }
 
-const TimeTableMobileView: React.FC<TimeTableSubViewProps> = ({ data }) => {
+interface ClassPopupProps {
+	data: TimeTableEntryFieldsForClass,
+	open: boolean,
+	onClose: () => void
+}
+
+const ClassPopup: React.FC<ClassPopupProps> = ({ data, open, onClose }) => {
 	const classes = useStyles();
-	
-	const entries = data.map((ef, i) => (
-		<TimeTableMobileViewEntry
-			key={i}
-			fields={ef}
-		/>
+
+	const Transition = React.forwardRef((
+		props: TransitionProps & { children?: React.ReactElement },
+		ref: React.Ref<unknown>
+	) => (
+		<Slide direction="up" {...props} ref={ref} />
 	));
 
 	return (
-		<div className={classes.list}>
-			{entries}
-		</div>
-	)
+		<Dialog fullScreen open={open} TransitionComponent={Transition} onClose={onClose}>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton edge="start" onClick={onClose} color="inherit"><CloseIcon /></IconButton>
+					<Typography variant="h6">{data.class.longName}</Typography>
+				</Toolbar>
+			</AppBar>
+			<main className={classes.dialogContent}>
+				<Typography variant="h5" gutterBottom className={classes.dialogHeading}>Einträge</Typography>
+				<List>
+					{data.fields.map((fields, i) => <TimeTableMobileViewEntry fields={fields} key={i} />)}
+				</List>
+			</main>
+		</Dialog>
+	);
+}
+
+const TimeTableMobileView: React.FC<TimeTableSubViewProps> = ({ data }) => {
+	const classes = useStyles();
+	const [currentClass, setCurrentClass] = React.useState<Class | null>(null);
+
+	const goToClass = (newClass: Class) => {
+		window.scrollTo({top: 0, behavior: "smooth"});
+		setCurrentClass(newClass);
+	}
+
+	const back = () => setCurrentClass(null)
+	
+	return (
+		<React.Fragment>
+			<List>
+				{data.map(({ class: cls, fields: { length } }, i) => (
+					<ListItem key={i} button onClick={() => goToClass(cls)} divider>
+						<ListItemText 
+							primary={cls.longName} 
+							secondary={`${length} ${length === 1 ? "Eintrag" : "Einträge"}`} 
+						/>
+					</ListItem>
+				))}
+			</List>
+			{currentClass && <ClassPopup 
+				data={data.find(d => d.class.shortName === currentClass.shortName) as TimeTableEntryFieldsForClass} 
+				onClose={back} 
+				open
+				key={currentClass.shortName}
+			/>}
+		</React.Fragment>
+	);
 }
 
 export default TimeTableMobileView;
