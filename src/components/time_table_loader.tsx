21f4d2dcbb6b1d85f290dfd38a5692e61e163be5
Ph4ntom3 <nicholas.roether@t-online.de>
2020-12-02 12:07:28 +0100

    Create TimeTableLoader

---
+++ src/components/time_table_loader.tsx
@@ -0,0 +1,19 @@
+import { Box, CircularProgress } from "@material-ui/core";
+import React from "react";
+import useSWR from "swr";
+import { getTable } from "../api_consume/consume_table";
+import TimeTableView from "./time_table_view";
+
+interface TimeTableLoaderProps {
+	date: Date | number | string
+}
+
+const TimeTableLoader: React.FC<TimeTableLoaderProps> = ({ date }) => {
+	if(!(date instanceof Date)) date = new  Date(date);
+	const res = useSWR(`table-${date.toISOString().split("T")[0]}`, () => getTable(date));
+	if(res.error) return <Box textAlign="center" mt={4}>Fehler beim Laden: {res.error}</Box>
+	if(!res.data) return <Box textAlign="center" mt={4}><CircularProgress /></Box>
+	return <TimeTableView table={res.data} />
+}
+
+export default TimeTableLoader;
