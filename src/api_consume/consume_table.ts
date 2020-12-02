aeba1a9a25a3deba3bfde04a56b440dc8b7b59a1
Ph4ntom3 <nicholas.roether@t-online.de>
2020-12-02 12:06:53 +0100

    Create getTable method

--- src/api_consume/consume_table.ts
+++ src/api_consume/consume_table.ts
@@ -10,19 +10,26 @@ function parseTable(data: any): TimeTable {
 	});
 }
 
+function handleResponse<T>(res: any): T {
+	if(!res) throw new Error("A communication error occured");
+	if(res.error) throw new Error(`Request failed: ${res.error}`);
+	if(!res.data) throw new Error("Not found");
+	return res.data as T;
+}
+
 async function getCurrentTables(num: number): Promise<TimeTable[] | null> {
-	const res = await fetch(API_BASE + `/current?num=${num}`).then(res => {
-		if(res.status != 200) return null;
-		return res.json()
-	});
-	const notFoundError = new Error("Table not found");
-	if(!res) throw notFoundError;
-	if(res.error) throw res.error;
-	const data = res.data;
-	if(data == null) throw notFoundError;
+	const res = await fetch(API_BASE + `/current?num=${num}`).then(res => res.json());
+	const data = handleResponse<any[]>(res);
 	return data.map((d: any) => parseTable(d));
 }
 
+async function getTable(date: Date | string | number) {
+	if(!(date instanceof Date)) date = new Date(date);
+	const res = await fetch(API_BASE + `/get?date=${date.getTime()}`).then(res => res.json());
+	return parseTable(handleResponse(res));
+}
+
 export {
-	getCurrentTables
+	getCurrentTables,
+	getTable
 }
