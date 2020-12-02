950388f249dd552f9941ebc5091bab9f22fc453b
nicholas-roether <nicholas.roether@t-online.de>
2020-12-02 14:18:21 +0100

    Set a table data refresh interval of 20s

--- src/components/time_table_loader.tsx
+++ src/components/time_table_loader.tsx
@@ -10,7 +10,9 @@ interface TimeTableLoaderProps {
 
 const TimeTableLoader: React.FC<TimeTableLoaderProps> = ({ date }) => {
 	if(!(date instanceof Date)) date = new Date(date);
-	const res = useSWR(`table-${date.toISOString().split("T")[0]}`, () => getTable(date));
+	const res = useSWR(`table-${date.toISOString().split("T")[0]}`, () => getTable(date), {
+		refreshInterval: 20000,
+	});
 	if(res.error) return <Box textAlign="center" mt={4}>Fehler beim Laden: {res.error}</Box>
 	if(!res.data) return <Box textAlign="center" mt={4}><CircularProgress /></Box>
 	return <TimeTableView table={res.data} />
