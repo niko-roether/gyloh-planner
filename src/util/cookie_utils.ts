e65cc4cbad675ef4eea5b102da3f4fb840b543fb
nicholas-roether <nicholas.roether@t-online.de>
2020-12-02 23:41:47 +0100

    Fix a bunch of stuff and create useCookie

--- src/util/cookie_utils.ts
+++ src/util/cookie_utils.ts
@@ -1,3 +1,7 @@
+import React from "react";
+
+const COOKIE_INDEFINITE = 378691200000;
+
 function setCookie(name: string, value: string, duration: number, path: string = location?.pathname || "/",) {
 	if(!document) throw Error("this function may only be executed on the client");
 	document.cookie = `${name}=${value}; expires=${new Date(Date.now() + duration)}; path=${path}; SameSite=Strict`
@@ -5,12 +9,24 @@ function setCookie(name: string, value: string, duration: number, path: string =
 
 function getCookie(name: string): string | null {
 	if(!document) throw Error("this function may only be executed on the client");
-	const regex = new RegExp(`${name}=(.+)(?:$|;)`);
+	const regex = new RegExp(`${name}+=([a-zA-Z0-9_\\- ]+)(?=;|$)`);
 	const res = regex.exec(document.cookie);
-	return res ? res[1] : null;
+	return res ? res[1] : "";
+}
+
+function useCookie(name: string): string | null {
+	const [value, setValue] = React.useState<string | null>(null);
+
+	React.useEffect(() => {
+		setValue(getCookie(name));
+	})
+
+	return value;
 }
 
 export {
 	setCookie,
-	getCookie
+	getCookie,
+	useCookie,
+	COOKIE_INDEFINITE
 }
