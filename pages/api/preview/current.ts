6f8683c2bbe01b92a88753f85bdc4ed2857a81e9
nicholas-roether <nicholas.roether@t-online.de>
2021-06-04 23:48:39 +0200

    Have preview be full page

--- pages/api/preview/current.ts
+++ pages/api/preview/current.ts
@@ -1,7 +1,7 @@
 import { NextApiHandler } from "next";
 
 const PreviewCurrent: NextApiHandler = async (req, res) => {
-	res.redirect(`https://api.apiflash.com/v1/urltoimage?access_key=${process.env.SCREENSHOT_API_KEY}&url=https://${req.headers.host}/show/current`)
+	res.redirect(`https://api.apiflash.com/v1/urltoimage?access_key=${process.env.SCREENSHOT_API_KEY}&full_page=true&url=https://${req.headers.host}/show/current`)
 }
 
 export default PreviewCurrent;
