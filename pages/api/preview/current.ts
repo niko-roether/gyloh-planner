d92a7565f54d26f3e76e560a95e66a851b49d724
nicholas-roether <nicholas.roether@t-online.de>
2021-06-05 00:09:52 +0200

    drastically lower preview resolution temporarily

--- pages/api/preview/current.ts
+++ pages/api/preview/current.ts
@@ -1,7 +1,7 @@
 import { NextApiHandler } from "next";
 
 const PreviewCurrent: NextApiHandler = async (req, res) => {
-	res.redirect(`https://api.apiflash.com/v1/urltoimage?access_key=${process.env.SCREENSHOT_API_KEY}&width=1280&height=720&url=https://${process.env.HOSTNAME}/show/current`)
+	res.redirect(`https://api.apiflash.com/v1/urltoimage?access_key=${process.env.SCREENSHOT_API_KEY}&width=200&height=150&url=https://${process.env.HOSTNAME}/show/current`)
 }
 
 export default PreviewCurrent;
